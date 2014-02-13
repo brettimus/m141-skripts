76c361d331156a9ccc97f70d4657778d599fe249
brettimus <brbeut@gmail.com>
2014-02-12 23:10:28 -0800

    Edited comments

--- batch_convert.r
+++ batch_convert.r
@@ -15,13 +15,13 @@ if (is.na(rmds[1])) {
   print("Could not find any .rmd labs. Are you sure you're in the right directory?")
 } else {
 
-  # Check for files without .RMD extension
+  # Record number of files without .RMD extension
   #
   all <- list.files()
   num_bad_labs <- length(all) - length(rmds)
 
 
-  # Create a directory for the .html files
+  # Create a directory for the .HTML files
   #
   html_dir <- file.path(getwd(),"labs_html")
   dir.create(html_dir)
@@ -30,7 +30,7 @@ if (is.na(rmds[1])) {
   #
   for (i in 1:length(rmds)) {
    
-    # Set output html path
+    # Create the output path, including the new .HTML filename
     #
     out <- file.path(html_dir,
                         paste(
@@ -39,14 +39,15 @@ if (is.na(rmds[1])) {
 			  sep=""
 			)
 		     )
-    # Convert "in" to html and place the output in our new directory
-    # How do we handle errors on this? Could run it quietly, I suppose...
+    # Finally, do the conversion
+    # (quiet suppresses output, but sometimes figures still load)
+    # !(should wrap this in a tryCatch at some point)
     #
     knitr::knit2html(input=rmds[i],output=out, quiet=T)
   }
 
   ################################################################################################
-  # BEGIN check for deviant files
+  # BEGIN the search for deviant files
   #
   if (num_bad_labs != 0) {
     
@@ -54,26 +55,26 @@ if (is.na(rmds[1])) {
     exts <- character()     # char vector for deviant filetypes
     
     for (i in 1:length(all)) {
+      # This is inefficient but it's fine since we only deal with max 50 files...
       if (!all[i] %in% rmds) {
         name <- strsplit(all[i],"_")[[1]][1]   # strsplit(..) returns a list
 	ext <- strsplit(all[i],"\\.")[[1]][2]  # NOTE use of "\\." for regex
-
+	
+	# Add the deviant to the vectors! Mwahaha!
 	names <- append(names,name)
 	exts <- append(exts,ext)
       }
      }
      
     # Create dataframe of deviants
-    #
     bad_eggs <- data.frame(NAMES=names,EXTS=exts, stringsAsFactors=F)
 
     # Alert user
-    #
     print(paste("There are",num_bad_labs,"labs with improper filetypes."))
     print("I shall list them for you:")
 
     for (i in 1:nrow(bad_eggs)) {
-      msg <- paste(bad_eggs[i,1]," submitted .",bad_eggs[i,2]," file", sep="")
+      msg <- paste(bad_eggs[i,1]," submitted a .",bad_eggs[i,2]," file", sep="")
       print(msg)
     }
   }
